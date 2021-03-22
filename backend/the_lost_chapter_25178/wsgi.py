0d0c22470add32bfa6fbf60b92f3b6066874faa9
Crowdbotics <team@crowdbotics.com>
2021-03-22 04:38:11 +0000

    Update models

--- backend/the_lost_chapter_25178/wsgi.py
+++ backend/the_lost_chapter_25178/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'the_lost_chapter_25178.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "the_lost_chapter_25178.settings")
 
 application = get_wsgi_application()
